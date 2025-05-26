24a68339dd0e75e695898ea6ac0acfb53ebd866f
Predrag Radic <pedja75@gmail.com>
2025-05-26 14:03:18 +0200

    Initial commit

---
+++ src/main/java/io/bootify/test_app_01/TestApp01Application.java
@@ -0,0 +1,14 @@
+package io.bootify.test_app_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TestApp01Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TestApp01Application.class, args);
+    }
+
+}
